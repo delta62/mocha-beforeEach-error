22287d64dd7d9934c8ad0074c94d2bdd538d2c6d
Sam Noedel <sam.noedel@changepoint.com>
2016-03-01 10:21:16 -0800

    Initial Commit

---
+++ test.js
@@ -0,0 +1,9 @@
+describe('My Test Suite', function() {
+    beforeEach(function() {
+        throw new Error('oh noes');
+    });
+
+    it('should do a thing', function() {
+        console.log('success!');
+    });
+});
